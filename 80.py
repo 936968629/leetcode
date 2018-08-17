6da6fe4e56b6693709af78d4d36d81d8c84b5126
wenjunlin <wenjunlin@100tal.com>
2018-08-17 17:24:00 +0800

    17

--- 80.py
+++ 80.py
@@ -4,23 +4,36 @@ class Solution:
         :type nums: List[int]
         :rtype: int
         """
-        i = 0
-        j = 1
-        count = 0
-        while j < len(nums):
-            if nums[i] == nums[j]:
-                count += 1
-                if count >= 2:
-                    del nums[j]
-                else:
-                    i += 1
-                    j += 1
-            else:
-                i += 1
-                j += 1
-                count = 0
+        # i = 0
+        # j = 1
+        # count = 0
+        # while j < len(nums):
+        #     if nums[i] == nums[j]:
+        #         count += 1
+        #         if count >= 2:
+        #             del nums[j]
+        #         else:
+        #             i += 1
+        #             j += 1
+        #     else:
+        #         i += 1
+        #         j += 1
+        #         count = 0
+        #
+        # print(nums)
 
-        print(nums)
+        '''
+        or
+        '''
+
+        i = 0
+        for n in nums:
+            if i < 2 or n > nums[i - 2]:
+                nums[i] = n
+                i += 1
+        print(i)
+        return i
+
 
 numsArr = [0,0,1,1,1,1,2,3,3]
 sol = Solution()
