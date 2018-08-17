36c589f43ff77f9ab5bd576c9b54231183432e72
wenjunlin <wenjunlin@100tal.com>
2018-08-17 16:07:19 +0800

    17

--- 26.py
+++ 26.py
@@ -5,6 +5,19 @@ class Solution:
         :rtype: int
         """
 
+        i = 0
+        j = 1
+        while i < len(nums):
+            if j >= len(nums):
+                break
+            if nums[i] == nums[j]:
+                del nums[j]
+            else:
+                i += 1
+                j += 1
 
+        print(nums)
 
+nums = [0,0,1,1,1,2,2,3,3,4]
 sol = Solution()
+sol.removeDuplicates(nums)
--- 27.py
+++ 27.py
@@ -6,11 +6,18 @@ class Solution:
         :rtype: int
         """
 
-        for index in reversed(range(len(nums))):
-            if nums[index] == val:
-                nums.remove(nums[index])
+        # for index in reversed(range(len(nums))):
+        #     if nums[index] == val:
+        #         nums.remove(nums[index])
 
+        # 删除list某一元素后对应的键值也会改变
 
+        i = 0
+        while i < len(nums):
+            if nums[i] == val:
+                del nums[i]
+            else:
+                i += 1
         print(nums)
 
 numArr = [0,1,2,2,3,0,4,2]
