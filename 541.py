203055b47a7014bad1cc937b9d36ab8019450fb0
wenjunlin <wenjunlin@100tal.com>
2018-08-23 20:51:55 +0800

    17

---
+++ 541.py
@@ -0,0 +1,24 @@
+class Solution:
+    def reverseStr(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: str
+        """
+        direction = -1
+        result = ''
+        for i in range(0, len(s), k):
+            if i + k < len(s):
+                result += s[i:k+i][::direction]
+            else:
+                result += s[i:k+i]
+            direction *= -1
+            # reverseStr = s[i:k][::-1]
+            # print(i)
+        print(result)
+        return result
+
+s = "abcdefg"
+k = 2
+sol = Solution()
+sol.reverseStr(s,k)
