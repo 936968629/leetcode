0779cf75eab2e4a5b57355c168015bbcd9febb65
wenjunlin <wenjunlin@100tal.com>
2018-08-24 13:59:04 +0800

    wen

--- 557.py
+++ 557.py
@@ -5,10 +5,14 @@ class Solution:
         :rtype: str
         """
         sList = s.split()
-        print(sList)
-        for index in range(len(sList)):
-            sList[index] = sList[index][::-1]
-        s = ' '.join(sList)
+        # for index in range(len(sList)):
+        #     sList[index] = sList[index][::-1]
+        # s = ' '.join(sList)
+        # print(s)
+        resuly = []
+        for item in sList:
+            resuly.append(item[::-1])
+        s = ' '.join(resuly)
         print(s)
         return s
 
