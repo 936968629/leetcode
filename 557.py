31fd9d7dd0d356c0d78c6cb7899c3c447f72ddb9
wenjunlin <wenjunlin@100tal.com>
2018-08-24 12:20:03 +0800

    wen

--- 557.py
+++ 557.py
@@ -4,7 +4,14 @@ class Solution:
         :type s: str
         :rtype: str
         """
+        sList = s.split()
+        print(sList)
+        for index in range(len(sList)):
+            sList[index] = sList[index][::-1]
+        s = ' '.join(sList)
+        print(s)
+        return s
 
-
-
+s = "Let's take LeetCode contes"
 sol = Solution()
+sol.reverseWords(s)
