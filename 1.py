760b63df7724d057f13545a2838e71abfde42f35
wenjunlin <wenjunlin@100tal.com>
2018-08-24 16:45:41 +0800

    wen

---
+++ 1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(len(nums)):
+            diff = target - nums[i]
+            if diff in nums[i+1:]:
+                print(i, nums[i+1:].index(diff) + 1 + i)
+                return (i, nums[i+1:].index(diff) + 1 + i)
+        print(-1, -1)
+        return (-1, -1)
+
+num = [2, 7, 4, 9]
+target = 14
+sol = Solution()
+sol.twoSum(num, target)
