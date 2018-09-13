daeeb76cd8ddac99c595ff47678b30910c0922cd
wenjunlin <wenjunlin@100tal.com>
2018-09-13 16:23:55 +0800

    init

--- 15.py
+++ 15.py
@@ -30,15 +30,55 @@ class Solution:
         print([list(i) for i in res])
         return [list(i) for i in res]
         """
-#       先排序
+        #       先排序
+        '''
         if len(nums) < 2:
             return []
         nums.sort()
-        print(nums)
+        res = []
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i - 1] == nums[i]: continue
+            l, r = i + 1, len(nums) - 1
+            while l < r:
+                s = nums[i] + nums[l] + nums[r]
+                if s == 0:
+                    res.append([nums[i], nums[l], nums[r]])
+                    l += 1
+                    r -= 1
+                    while l < r and nums[l] == nums[l - 1]: l += 1
+                    while l < r and nums[r] == nums[r + 1]: r -= 1
+                elif s < 0:
+                    l += 1
+                else:
+                    r -= 1
+        print(res)
+        return res
+        '''
+        if len(nums) < 2:
+            return []
+        nums.sort()
+        # res = []
+        resset = set()
+        for i in range(len(nums) - 2):
+            l, r = i+1, len(nums) - 1
+            while l < r:
+                s = nums[i] + nums[l] + nums[r]
+                if s == 0:
+                    # res.append([nums[i], nums[l], nums[r]])
+                    resset.add((nums[i], nums[l], nums[r]))
+                    l += 1
+                    r -= 1
+                elif s < 0:
+                    l += 1
+                else:
+                    r -= 1
+        # print(res)
+        # resset = list(resset)
+        print([list(index) for index in resset])
+        return resset
 
 
-
-oldNums = [-1, 0, 1, 2, -1, -4]
+oldNums = [-1, 0, 1, 2, -1, -4, -1]
 # oldNums.sort()
 sol = Solution()
 sol.threeSum(oldNums)
