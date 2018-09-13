7127ba8eb1acfb4c3701b54b6e9c78c2b58c4013
wenjunlin <wenjunlin@100tal.com>
2018-09-13 16:25:02 +0800

    init

--- 15.py
+++ 15.py
@@ -37,6 +37,7 @@ class Solution:
         nums.sort()
         res = []
         for i in range(len(nums) - 2):
+            #过滤相同的i
             if i > 0 and nums[i - 1] == nums[i]: continue
             l, r = i + 1, len(nums) - 1
             while l < r:
@@ -45,6 +46,7 @@ class Solution:
                     res.append([nums[i], nums[l], nums[r]])
                     l += 1
                     r -= 1
+                    #过滤相同的并列l,r
                     while l < r and nums[l] == nums[l - 1]: l += 1
                     while l < r and nums[r] == nums[r + 1]: r -= 1
                 elif s < 0:
