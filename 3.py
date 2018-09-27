ec78888f502df50bf2e4381356c2d6e51175baf9
936968629 <936968629@qq.com>
2018-09-27 23:23:55 +0800

    init

--- 3.py
+++ 3.py
@@ -7,21 +7,32 @@ class Solution:
         # [l, ... , r]滑动窗口
         l = 0
         r = -1
+        # 定义为最小
         res = 0
-        countList = {0} * 200
+        countList = {}
 
         # while l < len(s):
-        #     if countList.has_key(s [r+1])   and r + 1 < len(s):
+        #     if s[r+1] in countList.keys()  and r + 1 < len(s):
         #         r += 1
-        #
+        #         # countList[s[r]] = 1
         #     else:
-        #         # newlist.remove(s[l])
-        #         l = r
+        #         countList[s[r]] = 1
         #         l += 1
 
-        print(countList[s[r+1]])
-        # print(res)
-        return res
+
+        # print(countList)
+        # return res
+        cache = {}
+        longest = 0
+        start = 0
+        for i, ch in enumerate(s):
+            if ch in cache:
+                start = max(start, cache[ch] + 1)
+            cache[ch] = i
+            longest = max(longest, i - start + 1)
+
+        return longest
+
 
 
 str = 'bba'
