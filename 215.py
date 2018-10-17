708a9689ca7505029866e1d6e6fb0cda50e87c65
wenjunlin <wenjunlin@100tal.com>
2018-10-17 20:59:02 +0800

    'init'

--- 215.py
+++ 215.py
@@ -1,3 +1,4 @@
+import heapq
 class Solution(object):
     def findKthLargest(self, nums, k):
         """
@@ -5,10 +6,23 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
+        """
+        # 数组排序
         nums.sort()
         # print(nums)
         print(nums[len(nums) - k])
         return nums[len(nums) - k]
+        """
+        # 堆
+        heapList = nums[:k]
+        print(heapList)
+        heapq.heapify(heapList)
+        print(heapList)
+        for i in range(k, len(nums)):
+            if nums[i] >= heapList[0]:
+                heapq.heappushpop(heapList, nums[i])
+        print(heapList)
+        return heapList[0]
 
 
 nums = [3, 2, 1, 5, 6, 4, 4, 6]
